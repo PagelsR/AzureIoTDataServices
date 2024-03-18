15a2ca58494689864a8567f10e5240643701b2da
Randy Pagels <pagelsr@live.com>
2024-03-18 12:46:02 -0400

    Fix JSON formatting and function name in HubwayEventHubTriggerRead

--- FunctionApps/HubwayEventHubTrigger.cs
+++ FunctionApps/HubwayEventHubTrigger.cs
@@ -2,7 +2,8 @@ using System.Threading.Tasks;
 using Azure.Messaging.EventHubs;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Extensions.Logging;
-using Newtonsoft.Json.Linq;
+using System.Text.Json;
+//using Newtonsoft.Json.Linq;
 
 namespace FunctionApps
 {
@@ -21,26 +22,65 @@ namespace FunctionApps
             try
             {
                 // Parse the Event Hub message from JSON
-                dynamic msg = JObject.Parse(myEventHubMessage);
+                JsonDocument msg = JsonDocument.Parse(myEventHubMessage);
 
                 // Create the document to be written to Cosmos DB
                 outputDocument = new
+                // {
+                //     startTime = msg.RootElement.GetProperty("starttime").GetString(),
+                //     stopTime = msg.RootElement.GetProperty("stoptime").GetString(),
+                //     tripDuration = msg.RootElement.GetProperty("tripduration").GetString(),
+                //     startStationID = msg.RootElement.GetProperty("start station id").GetString(),
+                //     startStationName = msg.RootElement.GetProperty("start station name").GetString(),
+                //     startStationLatitiude = msg.RootElement.GetProperty("start station latitude").GetString(),
+                //     startStationLongitude = msg.RootElement.GetProperty("start station longitude").GetString(),
+                //     endStationID = msg.RootElement.GetProperty("end station id").GetString(),
+                //     endStationName = msg.RootElement.GetProperty("end station name").GetString(),
+                //     endStationLatitude = msg.RootElement.GetProperty("end station latitude").GetString(),
+                //     endStationLongitude = msg.RootElement.GetProperty("end station longitude").GetString(),
+                //     bikeID = msg.RootElement.GetProperty("bikeid").GetString(),
+                //     userType = msg.RootElement.GetProperty("usertype").GetString(),
+                //     gender = msg.RootElement.GetProperty("gender").GetString()
+                // };
+
                 {
-                    startTime = msg.GetValue("starttime")?.ToString(),
-                    stopTime = msg.GetValue("stoptime")?.ToString(),
-                    tripDuration = msg.GetValue("tripduration")?.ToString(),
-                    startStationID = msg.GetValue("start_station_id")?.ToString(),
-                    startStationName = msg.GetValue("start_station_name")?.ToString(),
-                    startStationLatitiude = msg.GetValue("start_station_latitude")?.ToString(),
-                    startStationLongitude = msg.GetValue("start_station_longitude")?.ToString(),
-                    endStationID = msg.GetValue("end_station_id")?.ToString(),
-                    endStationName = msg.GetValue("end_station_name")?.ToString(),
-                    endStationLatitude = msg.GetValue("end_station_latitude")?.ToString(),
-                    endStationLongitude = msg.GetValue("end_station_longitude")?.ToString(),
-                    bikeID = msg.GetValue("bikeid")?.ToString(),
-                    userType = msg.GetValue("usertype")?.ToString(),
-                    gender = msg.GetValue("gender")?.ToString()
+                    startTime = msg.RootElement.TryGetProperty("starttime", out JsonElement startTimeElement) ? startTimeElement.GetString() : null,
+                    stopTime = msg.RootElement.TryGetProperty("stoptime", out JsonElement stopTimeElement) ? stopTimeElement.GetString() : null,
+                    tripDuration = msg.RootElement.TryGetProperty("tripduration", out JsonElement tripDurationElement) ? tripDurationElement.GetString() : null,
+                    startStationID = msg.RootElement.TryGetProperty("start station id", out JsonElement startStationIDElement) ? startStationIDElement.GetString() : null,
+                    startStationName = msg.RootElement.TryGetProperty("start station name", out JsonElement startStationNameElement) ? startStationNameElement.GetString() : null,
+                    startStationLatitiude = msg.RootElement.TryGetProperty("start station latitude", out JsonElement startStationLatitiudeElement) ? startStationLatitiudeElement.GetString() : null,
+                    startStationLongitude = msg.RootElement.TryGetProperty("start station longitude", out JsonElement startStationLongitudeElement) ? startStationLongitudeElement.GetString() : null,
+                    endStationID = msg.RootElement.TryGetProperty("end station id", out JsonElement endStationIDElement) ? endStationIDElement.GetString() : null,
+                    endStationName = msg.RootElement.TryGetProperty("end station name", out JsonElement endStationNameElement) ? endStationNameElement.GetString() : null,
+                    endStationLatitude = msg.RootElement.TryGetProperty("end station latitude", out JsonElement endStationLatitudeElement) ? endStationLatitudeElement.GetString() : null,
+                    endStationLongitude = msg.RootElement.TryGetProperty("end station longitude", out JsonElement endStationLongitudeElement) ? endStationLongitudeElement.GetString() : null,
+                    bikeID = msg.RootElement.TryGetProperty("bikeid", out JsonElement bikeIDElement) ? bikeIDElement.GetString() : null,
+                    userType = msg.RootElement.TryGetProperty("usertype", out JsonElement userTypeElement) ? userTypeElement.GetString() : null,
+                    gender = msg.RootElement.TryGetProperty("gender", out JsonElement genderElement) ? genderElement.GetString() : null
                 };
+                
+                // // Parse the Event Hub message from JSON
+                // dynamic msg = JObject.Parse(myEventHubMessage);
+
+                // // Create the document to be written to Cosmos DB
+                // outputDocument = new
+                // {
+                //     startTime = msg.GetValue("starttime")?.ToString(),
+                //     stopTime = msg.GetValue("stoptime")?.ToString(),
+                //     tripDuration = msg.GetValue("tripduration")?.ToString(),
+                //     startStationID = msg.GetValue("start_station_id")?.ToString(),
+                //     startStationName = msg.GetValue("start_station_name")?.ToString(),
+                //     startStationLatitiude = msg.GetValue("start_station_latitude")?.ToString(),
+                //     startStationLongitude = msg.GetValue("start_station_longitude")?.ToString(),
+                //     endStationID = msg.GetValue("end_station_id")?.ToString(),
+                //     endStationName = msg.GetValue("end_station_name")?.ToString(),
+                //     endStationLatitude = msg.GetValue("end_station_latitude")?.ToString(),
+                //     endStationLongitude = msg.GetValue("end_station_longitude")?.ToString(),
+                //     bikeID = msg.GetValue("bikeid")?.ToString(),
+                //     userType = msg.GetValue("usertype")?.ToString(),
+                //     gender = msg.GetValue("gender")?.ToString()
+                // };
 
             }
             catch (System.Exception ex)
--- FunctionApps/HubwayEventHubTriggerRead.cs
+++ FunctionApps/HubwayEventHubTriggerRead.cs
@@ -11,7 +11,7 @@ namespace FunctionApps
 {
     public static class HubwayEventHubTriggerRead
     {
-        [FunctionName("cd "C:\Users\Rcd "C:\Users\RandyPagels\source\repos\AzureIoTDataServices\Lab 2 - Working with Hubway Data\Solution\IotProject\simulated-device\bin\Debug\net6.0"")]
+        [FunctionName("HubwayEventHubTriggerRead")]
         public static async Task Run([EventHubTrigger("hubwaytelemetry",
             Connection = "Shared_Access_Key_EVENTHUB",
             ConsumerGroup = "hubwaycg")] EventData[] events, ILogger log)
