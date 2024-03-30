016949d20f5e87291142da0cab24fe14e563d5c6
Randy Pagels <pagelsr@live.com>
2024-03-30 09:49:11 -0400

    Update delay time and connection string in simulated device code

--- Lab 2 - Working with Hubway Data/Solution/IotProject/simulated-device/SimulatedDevice.cs
+++ Lab 2 - Working with Hubway Data/Solution/IotProject/simulated-device/SimulatedDevice.cs
@@ -64,7 +64,10 @@ namespace simulated_device
                 await s_deviceClient.SendEventAsync(message);
                 Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, json);                
 
-                await Task.Delay(500);
+                await Task.Delay(200);
+
+                // Write a blank line
+                Console.WriteLine();
             }
         }
 
