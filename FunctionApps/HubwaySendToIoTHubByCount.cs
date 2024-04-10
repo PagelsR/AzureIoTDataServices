4285035dd283cd63b36cd1ed9e8c2e5a8b520292
Randy Pagels <pagelsr@live.com>
2024-04-10 15:26:13 -0500

    Refactor code and update dependencies in HubwaySendToIoTHubByCount.cs

--- FunctionApps/HubwaySendToIoTHubByCount.cs
+++ FunctionApps/HubwaySendToIoTHubByCount.cs
@@ -1,93 +1,98 @@
-using System;
-using System.IO;
-using System.Text;
-using System.Threading.Tasks;
-using Microsoft.Azure.Devices.Client;
-using Microsoft.Azure.WebJobs;
-using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.Extensions.Logging;
-using CsvHelper;
-using System.Collections.Generic;
-using System.Globalization;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+// using System;
+// using System.IO;
+// using System.Text;
+// using System.Threading.Tasks;
+// using Microsoft.Azure.Devices.Client;
+// using Microsoft.Azure.WebJobs;
+// using Microsoft.Azure.WebJobs.Extensions.Http;
+// using Microsoft.Extensions.Logging;
+// using CsvHelper;
+// using System.Collections.Generic;
+// using System.Globalization;
+// using Microsoft.AspNetCore.Http;
+// using Microsoft.AspNetCore.Mvc;
 
-public static class SimulatedIoTDevice_v3
-{
-    private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("Shared_Access_Key_IOTHUB"), TransportType.Mqtt);    
+// public static class SimulatedIoTDevice_v3
+// {
+//     private static readonly DeviceClient deviceClient = DeviceClient.CreateFromConnectionString(Environment.GetEnvironmentVariable("Shared_Access_Key_IOTHUB"), TransportType.Mqtt);    
 
-    [FunctionName("HubwaySendToIoTHubByCount")]
-    public static async Task<IActionResult> Run(
-            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
-            ILogger log, ExecutionContext context)
-    {
+//     [FunctionName("HubwaySendToIoTHubByCount")]
+//     public static async Task<IActionResult> Run(
+//             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+//             ILogger log, ExecutionContext context)
+//     {
 
-        log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+//         log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
         
-        string recordCountStr = req.Query["recordCount"];
-        int recordCount = string.IsNullOrEmpty(recordCountStr) ? 500 : int.Parse(recordCountStr);
+//         string recordCountStr = req.Query["recordCount"];
+//         int recordCount = string.IsNullOrEmpty(recordCountStr) ? 500 : int.Parse(recordCountStr);
 
-        await SendDeviceToCloudMessagesAsync(recordCount, context, log);
+//         log.LogInformation($"Attempting to send {recordCount} data items to IoT Hub...");
 
-        return new OkResult();
-    }
+//         await SendDeviceToCloudMessagesAsync(recordCount, context, log);
 
-    private static async Task SendDeviceToCloudMessagesAsync(int recordCount, ExecutionContext context, ILogger log)
-    {
-        log.LogInformation($"Starting to send {recordCount} records");
+//         //return new OkResult();
+//         string message = $"{recordCount} records sent to IoT Hub.";
+//         return new OkObjectResult(message);
 
-        int counter = 1;
+//     }
 
-        var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
-            {
-                HasHeaderRecord = true,
-                Delimiter = ","
-            };
+//     private static async Task SendDeviceToCloudMessagesAsync(int recordCount, ExecutionContext context, ILogger log)
+//     {
+//         log.LogInformation($"Starting to send {recordCount} records");
 
-        var sFilePath = Path.Combine(context.FunctionAppDirectory, "data", "201502-hubway-tripdata.csv");
-        using (var reader = new StreamReader(sFilePath))
-        using (var csv = new CsvReader(reader, config))
-        {
-            // csv.Configuration.HasHeaderRecord = true;
-            // csv.Configuration.Delimiter = ",";
-            var records = csv.GetRecords<dynamic>();
+//         int counter = 1;
+
+//         var config = new CsvHelper.Configuration.CsvConfiguration(CultureInfo.InvariantCulture)
+//             {
+//                 HasHeaderRecord = true,
+//                 Delimiter = ","
+//             };
+
+//         var sFilePath = Path.Combine(context.FunctionAppDirectory, "data", "201502-hubway-tripdata.csv");
+//         using (var reader = new StreamReader(sFilePath))
+//         using (var csv = new CsvReader(reader, config))
+//         {
+//             // csv.Configuration.HasHeaderRecord = true;
+//             // csv.Configuration.Delimiter = ",";
+//             var records = csv.GetRecords<dynamic>();
     
-            //int counter = 1;
+//             //int counter = 1;
     
-            foreach (var rec in records)
-            {
-                if (counter >= recordCount)
-                    break;
+//             foreach (var rec in records)
+//             {
+//                 if (counter >= recordCount)
+//                     break;
     
-                try
-                {
-                    string json = Newtonsoft.Json.JsonConvert.SerializeObject(rec);
+//                 try
+//                 {
+//                     string json = Newtonsoft.Json.JsonConvert.SerializeObject(rec);
     
-                    var message = new Message(Encoding.ASCII.GetBytes(json));
+//                     var message = new Message(Encoding.ASCII.GetBytes(json));
     
-                    message.Properties.Add("RoutingProperty", "Hubway");
+//                     message.Properties.Add("RoutingProperty", "Hubway");
     
-                    await deviceClient.SendEventAsync(message);
+//                     await deviceClient.SendEventAsync(message);
     
-                    counter++;
+//                     counter++;
     
-                    // Define messageString
-                    string messageString = Encoding.ASCII.GetString(message.GetBytes());
+//                     // Define messageString
+//                     string messageString = Encoding.ASCII.GetString(message.GetBytes());
 
 
-                    // Log information about the sent message
-                    log.LogInformation($"Sent message: {messageString}");
-                }
-                catch (Exception ex)
-                {
-                    // Log the exception
-                    log.LogError($"An error occurred: {ex.Message}");
-                }
-            }
+//                     // Log information about the sent message
+//                     log.LogInformation($"Sent message: {messageString}");
+//                 }
+//                 catch (Exception ex)
+//                 {
+//                     // Log the exception
+//                     log.LogError($"An error occurred: {ex.Message}");
+//                 }
+//             }
 
-            log.LogInformation($"Success sending {counter} records");
+//             log.LogInformation($"Success sending {counter} records");
 
-        }
-    }
+//         }
+//     }
 
-}
+// }
