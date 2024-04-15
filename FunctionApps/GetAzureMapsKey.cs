8abbc9108e13b46fd5c0a66028cb0bce48cf61a9
Randy Pagels <pagelsr@live.com>
2024-04-15 18:41:50 -0400

    Update Azure Maps API endpoint in HubwayMap HTML file

--- FunctionApps/GetAzureMapsKey.cs
+++ FunctionApps/GetAzureMapsKey.cs
@@ -1,14 +1,25 @@
-// C# Azure Function
-[FunctionName("GetAzureMapsKey")]
-public static async Task<IActionResult> Run(
-    [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
-    ILogger log)
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Extensions.Logging;
+
+public static class GetAzureMapsKey
 {
-    log.LogInformation("C# HTTP trigger function processed a request.");
+    // C# Azure Function
+    [FunctionName("GetAzureMapsKey")]
+    public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
+            ILogger log)
+    {
+        log.LogInformation("C# HTTP trigger function processed a request.");
 
-    var mapsKey = Environment.GetEnvironmentVariable("Azure_Maps_Subscription_Key");
+        var mapsKey = Environment.GetEnvironmentVariable("Azure_Maps_Subscription_Key");
 
-    log.LogInformation($"Retrieved MapsSubscriptionKey: {mapsKey}");
+        log.LogInformation($"Retrieved MapsSubscriptionKey: {mapsKey}");
 
-    return new OkObjectResult(mapsKey);
+        return new OkObjectResult(mapsKey);
+    }
 }
